b760d01537d27f52ed4ca70fe0ebc419d25e546b
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-27 11:14:42 -0700

    testing split with words array and innerHTML > list items

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* global words */
-/* eslint no-console: "off" */
+/* eslint no-console: "off"*/
 
 // get a random number between 0-8, then return matching word from array index
 function randoWord() {
@@ -14,13 +14,48 @@ function randoWord() {
 var guessMe = randoWord();
 console.log(guessMe.length, words);
 console.log(guessMe);
+
 var res = guessMe.split('');
 console.log(res);
-var letter1 = res[1];
-console.log(letter1);
 
+var letter1 = res[0];
+document.getElementById('letter-1').innerHTML = letter1;
+var letter2 = res[1];
+document.getElementById('letter-2').innerHTML = letter2;
+var letter3 = res[2];
+document.getElementById('letter-3').innerHTML = letter3;
+var letter4 = res[3];
+document.getElementById('letter-4').innerHTML = letter4;
 
+var letter5 = res[4];
+if(res[4] !== null) {
+    document.getElementById('letter-5').innerHTML = letter5;
+}
 
+var letter6 = res[5];
+if(res[5] !== null) {
+    document.getElementById('letter-6').innerHTML = letter6;
+}
+
+var letter7 = res[6];
+if(res[6] !== null) {
+    document.getElementById('letter-7').innerHTML = letter7;
+}
+
+var letter8 = res[7];
+if(res[7] !== null) {
+    document.getElementById('letter-8').innerHTML = letter8;
+
+    var letter9 = res[8];
+    if(res[8] !== null) {
+        document.getElementById('letter-9').innerHTML = letter9;
+    }
+
+    var letter10 = res[9];
+    if(res[9] !== null) {
+        document.getElementById('letter-10').innerHTML = letter10;
+    }
+}
 
 
 // loadWord function: randomly select from index
@@ -53,4 +88,4 @@ console.log(letter1);
  }
 } */
 
-// call 'loadWord' function to start things
+// call 'loadWord' function to start things /
